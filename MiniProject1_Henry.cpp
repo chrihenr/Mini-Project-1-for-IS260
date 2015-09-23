ca7620f5b9db72a1343c027753f18ee65155f92d
chrihenr <chrihenr@comfsm.fm>
2015-09-23 16:43:31 +1100

    Create MiniProject1_Henry.cpp

---
+++ MiniProject1_Henry.cpp
@@ -0,0 +1,13 @@
+// Submitted by:Christenson Henry
+// Submitted to: Edper Castro
+// Mini Project 1
+#include <iostream>
+#include <string>
+
+int main()
+{
+  std::string name;
+  std::cout << "What is your name? ";
+  getline (std::cin, name);
+  std::cout << "Hello, " << name << "!\n";
+}
