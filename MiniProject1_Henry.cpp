e3c0bf268aedbf3473cf6a59acf9ef2afdbdf296
chrihenr <chrihenr@comfsm.fm>
2015-09-23 17:09:49 +1100

    Update MiniProject1_Henry.cpp

--- MiniProject1_Henry.cpp
+++ MiniProject1_Henry.cpp
@@ -3,9 +3,20 @@
 // Mini Project 1
 #include <iostream>
 #include <string>
+using namespace std;
 
 int main()
 {
+    int demand;
+    const double a = 0.2;
+    
+    cout<<
+    
+    cout << "What are the demands?"<<endl;
+    cin>>demand;
+    cout<<endl;
+    
+    0.2 * 
   std::string name;
   std::cout << "What is your name? ";
   getline (std::cin, name);
